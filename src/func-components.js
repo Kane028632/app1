c4ae799ceaa6d1abfb15fdf9858cefc6d7f1c2a8
Kane028632 <kittapak5566@gmail.com>
2025-12-25 11:10:34 +0700

    Function Components
    create and add in app.js

---
+++ src/func-components.js
@@ -0,0 +1,35 @@
+import react from "react";
+
+export function Header() {
+    const aStlye = {
+        display: 'inline-block',
+        color: 'blue',
+        margin: '7px',
+        textDecoration: 'none'
+    }
+    return (
+        <div style={{backgroundColor:'#ccc'}} >
+            <a href="" style={aStlye}>Home</a> |&nbsp;
+            <a href="" style={aStlye}>React</a> |&nbsp;
+            <a href="" style={aStlye}>React Native </a>
+        </div>
+    )
+}
+
+export function Content() {
+    return (
+        <div style={{textAlign:'center'}}>
+            <h2>Hello<br/> React & React Native</h2>
+            <br/><br/><br/>
+        </div>
+    )
+}
+
+export const Footer = () => {
+    return (
+        <div style={{textAlign:'center'}}>
+            &copy; {new Date().getFullYear()} All rights reserved
+            </div>
+    )
+}
+    
