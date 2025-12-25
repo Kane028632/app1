0f35699e24355550df7438651c695a5c0af322e2
Kane028632 <kittapak5566@gmail.com>
2025-12-25 11:40:19 +0700

    class components
    create class components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 
 import react from 'react';
 import { Header, Content, Footer } from './func-components';
-
+import Celendar from './class-components';
 
 function App() {
   
   return (
-  [<Header></Header>,<Content></Content>,<Footer></Footer>]
+    <>
+  <Header></Header>
+  <p><center> <Celendar></Celendar> </center></p>
+  <Content></Content>
+  <Footer></Footer>
+  </>
  
   )
 }
