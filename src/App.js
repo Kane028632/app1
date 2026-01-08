7b77fd34941607f029a2459f87a853ea5641b177
Kane028632 <kittapak5566@gmail.com>
2026-01-08 10:26:42 +0700

    Event Handle
    Problem event handle

--- src/App.js
+++ src/App.js
@@ -1,19 +1,11 @@
 
 import react from 'react';
-import { Header, Content, Footer } from './func-components';
-import Celendar from './class-components';
+import Button from './class-components';
 
 function App() {
   
-  return (
-    <>
-  <Header></Header>
-  <p><center> <Celendar></Celendar> </center></p>
-  <Content></Content>
-  <Footer></Footer>
-  </>
- 
-  )
+  return <Button></Button>
+  
 }
 
 export default App;
