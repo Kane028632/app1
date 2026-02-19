2c4013ccf2c9884a1e176bbc902fe9647fa9be86
Kane028632 <85919604+Kane028632@users.noreply.github.com>
2026-02-19 11:50:09 +0700

    fromtext

--- src/home.js
+++ src/home.js
@@ -4,7 +4,7 @@ export default function Home() {
     return (
         <p style={{ textAlign: 'center' }}>
             <h3>ยินดีต้อนรับสู่ React Store   </h3>
-            รายชื่อ <a href="/products">สินค้า</a> ที่เรามีจ าหน่าย
+            รายชื่อ <a href="/products">สินค้า</a> ที่เรามีจำหน่าย
             <br />
             แต่ท่านต้องเป็น <a href="/member">สมาชิก</a> จึงจะ
             สั่งซื้อได้<br />
